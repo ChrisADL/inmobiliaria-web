c6a245948c981f8cc67a73945f06ba5e6caf85db
ChrisADL <chrisja_813@icloud.com>
2026-01-13 08:28:34 -0300

    Versión inicial del proyecto WEB IGS

---
+++ sw.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = 'gs-inmo-v1';
+
+const FILES_TO_CACHE = [
+  '/paginas/inicio/index.html',
+  '/paginas/inicio/styles.css',
+  '/public/manifest.json',
+  '/activos/img/icon-192.png',
+  '/activos/img/icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
